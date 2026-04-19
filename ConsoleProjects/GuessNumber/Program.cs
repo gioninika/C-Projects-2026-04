ef5e40b71e61f8d9f7a4b1658068c705bd4a8a6a
default <giocisco839@gmail.com>
2026-04-19 20:54:23 +0400

    GuessNumberV1

--- ConsoleProjects/GuessNumber/Program.cs
+++ ConsoleProjects/GuessNumber/Program.cs
@@ -4,7 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random rand = new Random();
+            bool resume = true;
+
+            while (resume)
+            {
+                int number = rand.Next(1, 101);
+                bool isincorrect = true;
+                int attempts = 0;
+
+                Console.WriteLine("gamoicani ricxvi 1 dan 100 mde!");
+
+                while (isincorrect)
+                {
+                    Console.Write("sheiyvane sheni varaundi: ");
+                    int guess = int.Parse(Console.ReadLine());
+
+                    attempts++;
+                    if (guess > number)
+                    {
+                        Console.WriteLine("dabalia!"); // სწორი რიცხვი დაბალია
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine("magalia!"); // სწორი რიცხვი მაღალია
+                    }
+                    else
+                    {
+                        Console.WriteLine($"gilocav! sworad gamoicani {attempts} mcdelobashi!");
+                        isincorrect = false;
+                    }
+                }
+
+                bool choiceresume = true;
+                while (choiceresume)
+                {
+                    Console.WriteLine("ginda tavidan? (Y/N):");
+                    string choice = Console.ReadKey().KeyChar.ToString().ToUpper();
+                    Console.WriteLine();
+
+                    if (choice == "Y")
+                    {
+                        Console.Clear();
+                        choiceresume = false;
+                    }
+                    else if (choice == "N")
+                    {
+                        Console.WriteLine("programa dasrulda.");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("shecdoma: daawire mxolod Y an N.");
+                    }
+                }
+            }
         }
     }
 }
